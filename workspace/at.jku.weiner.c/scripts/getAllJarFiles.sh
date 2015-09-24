a3fa5fffc693189067083f237aa312579a00359e
Harald Timeraider <timeraider@gmx.at>
2015-09-24 15:30:07 +0200

    moved to script directory

---
+++ workspace/at.jku.weiner.c/scripts/getAllJarFiles.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+MYPWD=$(pwd)
+OUT="./start-antlrworks.sh"
+PLUGINS="/tmp/jars/"
+
+echo "" > ${OUT}
+
+echo '#!/bin/bash' >> ${OUT}
+echo 'CLASSPATH="${CLASSPATH}:/usr/share/antlrworks/lib/antlrworks.jar"' >> ${OUT}
+echo 'CLASSPATH="${CLASSPATH}:/usr/share/antlr-3/lib/antlr3.jar"' >> ${OUT}
+echo 'CLASSPATH="${CLASSPATH}:/usr/share/jgoodies-forms/lib/forms.jar"' >> ${OUT}
+
+echo -n 'CLASSPATH="${CLASSPATH}:' >> ${OUT}
+echo -n "${PLUGINS}" >> ${OUT} 
+echo -n '/"' >> ${OUT}
+
+echo -n 'CLASSPATH="${CLASSPATH}:' >> ${OUT}
+echo -n "${PLUGINS}" >> ${OUT} 
+echo -n '/*"' >> ${OUT}
+
+
+#for FILE in $(find ${PLUGINS} -name "*.jar"); do
+	##echo -n '<listEntry value="&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;no&quot;?&gt;&#10;&lt;runtimeClasspathEntry externalArchive=&quot;/' >> ${OUT}
+	## echo ${MYPWD} >> ${OUT}
+	
+	#echo -n 'CLASSPATH="${CLASSPATH}:' >> ${OUT}
+	#echo -n ${FILE} >> ${OUT}
+	#echo -n '"' >> ${OUT}
+	
+	##echo -n '&quot; path=&quot;3&quot; type=&quot;2&quot;/&gt;&#10;"/>' >> ${OUT}
+#	echo "" >> ${OUT}
+#done
+
+echo 'export CLASSPATH="${CLASSPATH}:../bin/:."' >> ${OUT}
+
+#echo 'java -cp ${CLASSPATH}:. org.antlr.works.IDE' >> ${OUT}
+echo 'java org.antlr.works.IDE' >> ${OUT}
+
+
+chmod u+x ${OUT}

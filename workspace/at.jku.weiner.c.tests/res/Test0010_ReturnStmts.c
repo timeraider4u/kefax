aa2defa2a9c0d59e877fa515e4906528f4e220cc
timeraider@gmx.at <timeraider@gmx.at>
2015-10-18 03:11:15 +0200

    added constants

---
+++ workspace/at.jku.weiner.c.tests/res/Test0010_ReturnStmts.c
@@ -0,0 +1,19 @@
+char doSomething1() {
+	return 'a';
+}
+
+int doSomething2() {
+	return -1;
+}
+
+double doSomething3() {
+	return 0.8;
+}
+
+float doSomething4() {
+	return 0x011;
+}
+
+void doSomething5() {
+	return;
+}

0ab5455b5db2bf34c4fea2dbc82fb92131b5f945
timeraider@gmx.at <timeraider@gmx.at>
2015-10-23 20:26:26 +0200

    test case 0029 updated (work in progress)

--- workspace/at.jku.weiner.c.tests/res/Test0029_Assembler.c
+++ workspace/at.jku.weiner.c.tests/res/Test0029_Assembler.c
@@ -7,5 +7,11 @@ extern int errno;
 int funcname(int arg1, int *arg2, int arg3)
 {
 	int res;
+	/* The operating system will return a negative value on error;
+	 * wrappers return -1 on error and set the errno global variable */
+	if (-125	<=	res && res < 0) {
+		errno	=	-res;
+		res		=	-1;
+	}
 	return res;
 }

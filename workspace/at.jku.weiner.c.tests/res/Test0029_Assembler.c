867ba769a5d74ed6fcbe78cd6e22cee658fa4dbd
timeraider@gmx.at <timeraider@gmx.at>
2015-10-23 14:28:41 +0200

    updated test 0029 assembler (work in progress)

--- workspace/at.jku.weiner.c.tests/res/Test0029_Assembler.c
+++ workspace/at.jku.weiner.c.tests/res/Test0029_Assembler.c
@@ -3,3 +3,7 @@
  * Copyright (2015) Wikipedia authors - WP:CC BY-SA
  */
 extern int errno;
+
+int funcname(int arg1, int *arg2, int arg3)
+{
+}
--- workspace/at.jku.weiner.c.tests/src-gen/at/jku/weiner/c/xtexttests/Test0029_Assembler.java
+++ workspace/at.jku.weiner.c.tests/src-gen/at/jku/weiner/c/xtexttests/Test0029_Assembler.java
@@ -35,6 +35,34 @@ import at.jku.weiner.c.c.InitDeclaratorList;
 import at.jku.weiner.c.c.InitDeclarator;
 import at.jku.weiner.c.c.Declarator;
 import at.jku.weiner.c.c.DirectDeclarator;
+import at.jku.weiner.c.c.ExternalDeclaration;
+import at.jku.weiner.c.c.FunctionDefHead;
+import at.jku.weiner.c.c.DeclarationSpecifiers;
+import at.jku.weiner.c.c.TypeSpecifier;
+import at.jku.weiner.c.c.Declarator;
+import at.jku.weiner.c.c.DirectDeclarator;
+import at.jku.weiner.c.c.DeclaratorSuffix;
+import at.jku.weiner.c.c.DirectDeclaratorLastSuffix;
+import at.jku.weiner.c.c.ParameterTypeList;
+import at.jku.weiner.c.c.ParameterList;
+import at.jku.weiner.c.c.ParameterDeclaration;
+import at.jku.weiner.c.c.DeclarationSpecifiers;
+import at.jku.weiner.c.c.TypeSpecifier;
+import at.jku.weiner.c.c.Declarator;
+import at.jku.weiner.c.c.DirectDeclarator;
+import at.jku.weiner.c.c.ParameterDeclaration;
+import at.jku.weiner.c.c.DeclarationSpecifiers;
+import at.jku.weiner.c.c.TypeSpecifier;
+import at.jku.weiner.c.c.Declarator;
+import at.jku.weiner.c.c.DirectDeclarator;
+import at.jku.weiner.c.c.Pointer;
+import at.jku.weiner.c.c.ParameterDeclaration;
+import at.jku.weiner.c.c.DeclarationSpecifiers;
+import at.jku.weiner.c.c.TypeSpecifier;
+import at.jku.weiner.c.c.Declarator;
+import at.jku.weiner.c.c.DirectDeclarator;
+import at.jku.weiner.c.c.FunctionDefinition;
+import at.jku.weiner.c.c.BodyStatement;
 
 @SuppressWarnings("unused")
 @RunWith(XtextRunner.class)
@@ -81,6 +109,31 @@ public class Test0029_Assembler {
 			"RULE_ID",
 			"RULE_SEMI",
 			"RULE_NEWLINE",
+			"RULE_NEWLINE",
+			"RULE_KW_INT",
+			"RULE_WHITESPACE",
+			"RULE_ID",
+			"RULE_LEFTPAREN",
+			"RULE_KW_INT",
+			"RULE_WHITESPACE",
+			"RULE_ID",
+			"RULE_COMMA",
+			"RULE_WHITESPACE",
+			"RULE_KW_INT",
+			"RULE_WHITESPACE",
+			"RULE_STAR",
+			"RULE_ID",
+			"RULE_COMMA",
+			"RULE_WHITESPACE",
+			"RULE_KW_INT",
+			"RULE_WHITESPACE",
+			"RULE_ID",
+			"RULE_RIGHTPAREN",
+			"RULE_NEWLINE",
+			"RULE_LEFTBRACE",
+			"RULE_NEWLINE",
+			"RULE_RIGHTBRACE",
+			"RULE_NEWLINE",
 		};
 		//final List<Token> actual = testHelper.getTokens(text);
 		//testHelper.outputTokens(text);
@@ -103,7 +156,7 @@ public class Test0029_Assembler {
 		Assert.assertNotNull(TranslationUnit_1_Var);
 		final EList<? extends EObject> External_1_list = TranslationUnit_1_Var.getExternal();
 		Assert.assertNotNull(External_1_list);
-		Assert.assertEquals(1, External_1_list.size());
+		Assert.assertEquals(2, External_1_list.size());
 		final ExternalDeclaration ExternalDeclaration_2_Var = (ExternalDeclaration)External_1_list.get(0);
 		Assert.assertNotNull(ExternalDeclaration_2_Var);
 		final Declaration Declaration_3_Var = (Declaration)ExternalDeclaration_2_Var.getDeclaration();
@@ -135,6 +188,92 @@ public class Test0029_Assembler {
 		Assert.assertNotNull(DirectDeclarator_10_Var);
 		Assert.assertEquals("errno", DirectDeclarator_10_Var.getId());
 		Assert.assertEquals(";", Declaration_3_Var.getSemi());
+		final ExternalDeclaration ExternalDeclaration_11_Var = (ExternalDeclaration)External_1_list.get(1);
+		Assert.assertNotNull(ExternalDeclaration_11_Var);
+		final FunctionDefHead FunctionDefHead_12_Var = (FunctionDefHead)ExternalDeclaration_11_Var.getFunctiondefHead();
+		Assert.assertNotNull(FunctionDefHead_12_Var);
+		final DeclarationSpecifiers DeclarationSpecifiers_13_Var = (DeclarationSpecifiers)FunctionDefHead_12_Var.getFunDeclSpecifiers();
+		Assert.assertNotNull(DeclarationSpecifiers_13_Var);
+		final EList<? extends EObject> DeclarationSpecifier_13_list = DeclarationSpecifiers_13_Var.getDeclarationSpecifier();
+		Assert.assertNotNull(DeclarationSpecifier_13_list);
+		Assert.assertEquals(1, DeclarationSpecifier_13_list.size());
+		final TypeSpecifier TypeSpecifier_14_Var = (TypeSpecifier)DeclarationSpecifier_13_list.get(0);
+		Assert.assertNotNull(TypeSpecifier_14_Var);
+		Assert.assertEquals("int", TypeSpecifier_14_Var.getName());
+		final Declarator Declarator_15_Var = (Declarator)FunctionDefHead_12_Var.getFunDeclarator();
+		Assert.assertNotNull(Declarator_15_Var);
+		final DirectDeclarator DirectDeclarator_16_Var = (DirectDeclarator)Declarator_15_Var.getDeclarator();
+		Assert.assertNotNull(DirectDeclarator_16_Var);
+		Assert.assertEquals("funcname", DirectDeclarator_16_Var.getId());
+		final EList<? extends EObject> DeclaratorSuffix_16_list = DirectDeclarator_16_Var.getDeclaratorSuffix();
+		Assert.assertNotNull(DeclaratorSuffix_16_list);
+		Assert.assertEquals(1, DeclaratorSuffix_16_list.size());
+		final DeclaratorSuffix DeclaratorSuffix_17_Var = (DeclaratorSuffix)DeclaratorSuffix_16_list.get(0);
+		Assert.assertNotNull(DeclaratorSuffix_17_Var);
+		final DirectDeclaratorLastSuffix DirectDeclaratorLastSuffix_18_Var = (DirectDeclaratorLastSuffix)DeclaratorSuffix_17_Var.getLastSuffix();
+		Assert.assertNotNull(DirectDeclaratorLastSuffix_18_Var);
+		final EList<? extends EObject> ParameterTypeList_18_list = DirectDeclaratorLastSuffix_18_Var.getParameterTypeList();
+		Assert.assertNotNull(ParameterTypeList_18_list);
+		Assert.assertEquals(1, ParameterTypeList_18_list.size());
+		final ParameterTypeList ParameterTypeList_19_Var = (ParameterTypeList)ParameterTypeList_18_list.get(0);
+		Assert.assertNotNull(ParameterTypeList_19_Var);
+		final ParameterList ParameterList_20_Var = (ParameterList)ParameterTypeList_19_Var.getList();
+		Assert.assertNotNull(ParameterList_20_Var);
+		final EList<? extends EObject> ParameterDeclaration_20_list = ParameterList_20_Var.getParameterDeclaration();
+		Assert.assertNotNull(ParameterDeclaration_20_list);
+		Assert.assertEquals(3, ParameterDeclaration_20_list.size());
+		final ParameterDeclaration ParameterDeclaration_21_Var = (ParameterDeclaration)ParameterDeclaration_20_list.get(0);
+		Assert.assertNotNull(ParameterDeclaration_21_Var);
+		final DeclarationSpecifiers DeclarationSpecifiers_22_Var = (DeclarationSpecifiers)ParameterDeclaration_21_Var.getDeclSpecifiers();
+		Assert.assertNotNull(DeclarationSpecifiers_22_Var);
+		final EList<? extends EObject> DeclarationSpecifier_22_list = DeclarationSpecifiers_22_Var.getDeclarationSpecifier();
+		Assert.assertNotNull(DeclarationSpecifier_22_list);
+		Assert.assertEquals(1, DeclarationSpecifier_22_list.size());
+		final TypeSpecifier TypeSpecifier_23_Var = (TypeSpecifier)DeclarationSpecifier_22_list.get(0);
+		Assert.assertNotNull(TypeSpecifier_23_Var);
+		Assert.assertEquals("int", TypeSpecifier_23_Var.getName());
+		final Declarator Declarator_24_Var = (Declarator)ParameterDeclaration_21_Var.getDeclarator();
+		Assert.assertNotNull(Declarator_24_Var);
+		final DirectDeclarator DirectDeclarator_25_Var = (DirectDeclarator)Declarator_24_Var.getDeclarator();
+		Assert.assertNotNull(DirectDeclarator_25_Var);
+		Assert.assertEquals("arg1", DirectDeclarator_25_Var.getId());
+		final ParameterDeclaration ParameterDeclaration_26_Var = (ParameterDeclaration)ParameterDeclaration_20_list.get(1);
+		Assert.assertNotNull(ParameterDeclaration_26_Var);
+		final DeclarationSpecifiers DeclarationSpecifiers_27_Var = (DeclarationSpecifiers)ParameterDeclaration_26_Var.getDeclSpecifiers();
+		Assert.assertNotNull(DeclarationSpecifiers_27_Var);
+		final EList<? extends EObject> DeclarationSpecifier_27_list = DeclarationSpecifiers_27_Var.getDeclarationSpecifier();
+		Assert.assertNotNull(DeclarationSpecifier_27_list);
+		Assert.assertEquals(1, DeclarationSpecifier_27_list.size());
+		final TypeSpecifier TypeSpecifier_28_Var = (TypeSpecifier)DeclarationSpecifier_27_list.get(0);
+		Assert.assertNotNull(TypeSpecifier_28_Var);
+		Assert.assertEquals("int", TypeSpecifier_28_Var.getName());
+		final Declarator Declarator_29_Var = (Declarator)ParameterDeclaration_26_Var.getDeclarator();
+		Assert.assertNotNull(Declarator_29_Var);
+		final DirectDeclarator DirectDeclarator_30_Var = (DirectDeclarator)Declarator_29_Var.getDeclarator();
+		Assert.assertNotNull(DirectDeclarator_30_Var);
+		Assert.assertEquals("arg2", DirectDeclarator_30_Var.getId());
+		final Pointer Pointer_31_Var = (Pointer)Declarator_29_Var.getPointer();
+		Assert.assertNotNull(Pointer_31_Var);
+		Assert.assertEquals("[*]", Pointer_31_Var.getStar().toString());
+		final ParameterDeclaration ParameterDeclaration_32_Var = (ParameterDeclaration)ParameterDeclaration_20_list.get(2);
+		Assert.assertNotNull(ParameterDeclaration_32_Var);
+		final DeclarationSpecifiers DeclarationSpecifiers_33_Var = (DeclarationSpecifiers)ParameterDeclaration_32_Var.getDeclSpecifiers();
+		Assert.assertNotNull(DeclarationSpecifiers_33_Var);
+		final EList<? extends EObject> DeclarationSpecifier_33_list = DeclarationSpecifiers_33_Var.getDeclarationSpecifier();
+		Assert.assertNotNull(DeclarationSpecifier_33_list);
+		Assert.assertEquals(1, DeclarationSpecifier_33_list.size());
+		final TypeSpecifier TypeSpecifier_34_Var = (TypeSpecifier)DeclarationSpecifier_33_list.get(0);
+		Assert.assertNotNull(TypeSpecifier_34_Var);
+		Assert.assertEquals("int", TypeSpecifier_34_Var.getName());
+		final Declarator Declarator_35_Var = (Declarator)ParameterDeclaration_32_Var.getDeclarator();
+		Assert.assertNotNull(Declarator_35_Var);
+		final DirectDeclarator DirectDeclarator_36_Var = (DirectDeclarator)Declarator_35_Var.getDeclarator();
+		Assert.assertNotNull(DirectDeclarator_36_Var);
+		Assert.assertEquals("arg3", DirectDeclarator_36_Var.getId());
+		final FunctionDefinition FunctionDefinition_37_Var = (FunctionDefinition)ExternalDeclaration_11_Var.getFunctionDefinition();
+		Assert.assertNotNull(FunctionDefinition_37_Var);
+		final BodyStatement BodyStatement_38_Var = (BodyStatement)FunctionDefinition_37_Var.getBody();
+		Assert.assertNotNull(BodyStatement_38_Var);
 	}
 
 

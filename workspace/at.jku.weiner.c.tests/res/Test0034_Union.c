77d7a40f491b9c78a5e24e32e92daa11b36c1b26
timeraider@gmx.at <timeraider@gmx.at>
2015-10-26 02:18:10 +0100

    updated test case 0034 union a little bit

--- workspace/at.jku.weiner.c.tests/res/Test0034_Union.c
+++ workspace/at.jku.weiner.c.tests/res/Test0034_Union.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
+	// unsigned long size = (unsigned long)sizeof(number);
+	printf("sizeof(number)=%lu\n", size);
+	//printf("sizeof(number.numChar)=%lu\n", sizeof(number.numChar));
+	printf("sizeof(float)=%lu\n", sizeof(float));
+	// number.numChar = 65;
+	printf("number.numChar(as decimal)=%d\n", number.numChar);
+	printf("number.numChar(as char)=%c\n", number.numChar);
+	printf("number.numFloat=%f\n", number.numFloat);
+	// number.numFloat = 0.56789;
+	printf("number.numChar(as decimal)=%d\n", number.numChar);
 	printf("number.numFloat=%f\n", number.numFloat);
 	return 0;
 }

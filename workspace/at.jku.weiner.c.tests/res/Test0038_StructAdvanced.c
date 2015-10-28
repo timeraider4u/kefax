62ece7488870a66f8db7a509ff84320b84c733e6
timeraider@gmx.at <timeraider@gmx.at>
2015-10-28 01:02:51 +0100

    test 0038 work in progress

---
+++ workspace/at.jku.weiner.c.tests/res/Test0038_StructAdvanced.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct empStruct {
+	int id;
+	char name[250];
+} employee;
+
+struct empStruct joe = {0x007, "Joe Nobody"};
+employee phil = {0x008, "Phil Morrison"};
+
+void print(employee *emp) {
+	printf("(*employee).id=%d\n", (*emp).id);
+	// printf("employee->name=%s\n", emp->name);
+}
+
+int main(void) {
+	// employee * newEmployee = malloc(sizeof(employee));
+	if (newEmployee == NULL) {
+		printf("Could not reserve space for new employee!\n");
+		return -1;
+	}
+	print(&joe);
+	print(&phil);
+	// newEmployee->id = 0x009;
+	// strcpy(newEmployee->name, "Billy The Kid");
+	print(newEmployee);
+	free(newEmployee);
+	return 0;
+}
---
+++ workspace/at.jku.weiner.c.tests/src/at/jku/weiner/c/xtexttests/TestSuite_Structs.java
@@ -0,0 +1,12 @@
+package at.jku.weiner.c.xtexttests;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Test0037_Struct.class, Test0038_StructAdvanced.class })
+public class TestSuite_Structs {
+
+}

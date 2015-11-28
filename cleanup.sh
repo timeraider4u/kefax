ebf26df3454616f93f6459a9a4a4740cad75e192
timeraider@gmx.at <timeraider@gmx.at>
2015-11-28 05:57:43 +0100

    more clean-up

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# cleanup.sh
+
+DIR="./workspace"
+function add() {
+	for FILE in $(find "${DIR}" -name "${1}" -type d); do
+		touch $FILE/"${2}"
+		git add --force ${FILE}/"${2}"
+	done
+}
+
+add "bin" ".keep_binary"
+add "src-gen" ".keep"
+add "xtend-gen" ".keep"
+
+rm -r ${DIR}/at.jku.weiner.c.{parser,preprocess}.{ui,ui.tests}

6a48cb8e4ff1d296156382e9547d53d21a2364d5
Harald Timeraider <timeraider@gmx.at>
2015-12-09 20:01:32 +0100

    added ui and ui.test projects (needed by MoDisco discoverer)

--- cleanup.sh
+++ cleanup.sh
@@ -13,4 +13,4 @@ add "bin" ".keep_binary"
 add "src-gen" ".keep_src_gen"
 add "xtend-gen" ".keep_xtend_gen"
 
-rm -r ${DIR}/at.jku.weiner.c.{parser,preprocess}.{ui,ui.tests}
+#rm -r ${DIR}/at.jku.weiner.c.{parser,preprocess}.{ui,ui.tests}

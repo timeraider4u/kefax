43ab9dd4c918740550f72b66b4b9a6ed3313155b
timeraider@gmx.at <timeraider@gmx.at>
2015-12-16 21:45:40 +0100

    added a test case for function-macro evaluation inside of if-directives

---
+++ workspace/at.jku.weiner.c.preprocess.tests/res/Test0072_IfMin.c
@@ -0,0 +1,4 @@
+#define MIN(X, Y)	((X) < (Y) ? (X) : (Y))
+#if MIN(0,6) >= 5
+	void greaterEqualFive();
+#endif
---
+++ workspace/at.jku.weiner.c.preprocess.tests/res/Test0073_IfMin.c
@@ -0,0 +1,4 @@
+#define MIN(X, Y)	((X) < (Y) ? (X) : (Y))
+#if MIN(5,6) >= 5
+	void greaterEqualFive();
+#endif

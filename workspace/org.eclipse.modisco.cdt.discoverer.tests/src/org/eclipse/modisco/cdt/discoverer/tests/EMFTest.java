9a61e801e53ede880788eef14af69b33601009b7
timeraider@gmx.at <timeraider@gmx.at>
2015-12-31 02:06:05 +0100

    removed unnecessary imports

--- workspace/org.eclipse.modisco.cdt.discoverer.tests/src/org/eclipse/modisco/cdt/discoverer/tests/EMFTest.java
+++ workspace/org.eclipse.modisco.cdt.discoverer.tests/src/org/eclipse/modisco/cdt/discoverer/tests/EMFTest.java
@@ -4,21 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 
 import at.jku.weiner.c.common.common.Model;
 
 public class EMFTest {
-
+	
 	public static Model emfTest(final String pureJavaClassFileName,
 			final String sourceFile) throws Exception {
 		final TestUtils utils = new TestUtils(sourceFile);
 		final Model result = utils.getModel();
 		return result;
 	}
-
+	
 	public static Map<String, Object> getOptions(
 			final String pureJavaClassFileName, final String sourceFile) {
 		final Map<String, Object> result = new HashMap<String, Object>();
@@ -30,5 +28,5 @@ public class EMFTest {
 		result.put("path", path2);
 		return result;
 	}
-
+	
 }

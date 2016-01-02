eb8f98a919906ff4dba70e44033541184a22c6c6
timeraider@gmx.at <timeraider@gmx.at>
2016-01-02 02:21:38 +0100

    added another test case to see if parsing of single file is also possible

--- workspace/org.eclipse.modisco.cdt.discoverer.tests/src/org/eclipse/modisco/cdt/discoverer/tests/TestUtils.java
+++ workspace/org.eclipse.modisco.cdt.discoverer.tests/src/org/eclipse/modisco/cdt/discoverer/tests/TestUtils.java
@@ -1,6 +1,5 @@
 package org.eclipse.modisco.cdt.discoverer.tests;
 
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -12,28 +11,23 @@ import org.junit.Assert;
 import at.jku.weiner.c.common.common.Model;
 
 public class TestUtils {
-	
+
 	private final IProject project;
 	private final DiscoverCDTFromIResource discoverer;
 	private final Resource discoveredModel;
-	
+
 	public TestUtils(final IProject iProject, final String testName)
 			throws Exception {
 		this.project = iProject;
 		Assert.assertNotNull(this.project);
-		final IResource srcFolder = this.getRes(testName + "/");
-		Assert.assertNotNull(srcFolder);
+		final IResource res = this.getRes(testName);
+		Assert.assertNotNull(res);
 		this.discoverer = new DiscoverCDTFromIResource();
-		Assert.assertTrue(
-				"res is not an instanceof IFolder, res='" + srcFolder.getName()
-				+ "'", srcFolder instanceof IFolder);
-		final IFolder srcFolderDir = (IFolder) srcFolder;
-		this.discoverer
-		.discoverElement(srcFolderDir, new NullProgressMonitor());
+		this.discoverer.discoverElement(res, new NullProgressMonitor());
 		this.discoveredModel = this.discoverer.getTargetModel();
 		Assert.assertNotNull(this.discoveredModel);
 	}
-	
+
 	private IResource getRes(final String fileName) {
 		IResource res = this.project.getFile(fileName);
 		if ((res == null) || !res.exists()) {
@@ -45,11 +39,11 @@ public class TestUtils {
 				res.exists() && res.isAccessible());
 		return res;
 	}
-	
+
 	public Model getModel() {
 		final EObject root = this.discoveredModel.getContents().get(0);
 		final Model model = (Model) root;
 		return model;
 	}
-	
+
 }

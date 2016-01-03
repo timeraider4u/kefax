7db0a5b77ad56e5c258c76fe21b79064066d23e1
timeraider@gmx.at <timeraider@gmx.at>
2016-01-03 23:09:32 +0100

    update of kefax

---
+++ workspace/at.jku.isse.ecco.kefax.main/src/at/jku/isse/ecco/kefax/main/startup/CmdArgs.java
@@ -0,0 +1,103 @@
+package at.jku.isse.ecco.kefax.main.startup;
+
+import org.eclipse.emf.common.util.EList;
+
+import at.jku.weiner.c.cmdarguments.cmdArgs.Argument;
+import at.jku.weiner.c.cmdarguments.cmdArgs.CmdLine;
+import at.jku.weiner.c.cmdarguments.cmdArgs.FunctionMacro;
+import at.jku.weiner.c.cmdarguments.cmdArgs.Macro;
+import at.jku.weiner.c.cmdarguments.cmdArgs.ObjectMacro;
+import at.jku.weiner.c.cmdarguments.cmdArgs.SimpleMacro;
+
+public class CmdArgs {
+	
+	private final String path;
+	private final CmdLine line;
+	
+	private final StringBuffer additionalDirectives;
+
+	public CmdArgs(final String path, final CmdLine line) {
+		this.path = path;
+		this.line = line;
+		this.additionalDirectives = new StringBuffer("");
+		
+		this.visit(this.line.getArguments());
+	}
+
+	private final void visit(final EList<Argument> list) {
+		if ((list == null) || list.isEmpty()) {
+			this.error("list of arguments should contain entries, cmdPath='"
+					+ this.path + "'");
+		}
+		for (int i = 0; i < list.size(); i++) {
+			final Argument arg = list.get(0);
+			this.visit(arg);
+		}
+	}
+
+	private final void visit(final Argument arg) {
+		final Macro macro = arg.getMacro();
+		if (macro != null) {
+			this.visit(macro);
+		}
+	}
+	
+	private final void visit(final Macro macro) {
+		System.out.println("visitMacro='" + macro + "'");
+		if (macro instanceof SimpleMacro) {
+			this.visitFor((SimpleMacro) macro);
+		} else if (macro instanceof ObjectMacro) {
+			this.visitFor((ObjectMacro) macro);
+		} else {
+			this.visitFor((FunctionMacro) macro);
+		}
+	}
+
+	private final void visitFor(final SimpleMacro macro) {
+		this.additionalDirectives.append("#define ");
+		this.additionalDirectives.append(macro.getName());
+		this.additionalDirectives.append(System.lineSeparator());
+	}
+
+	private final void visitFor(final ObjectMacro macro) {
+		this.additionalDirectives.append("#define ");
+		this.additionalDirectives.append(macro.getName());
+		this.additionalDirectives.append(" ");
+		this.additionalDirectives.append(macro.getValue());
+		this.additionalDirectives.append(System.lineSeparator());
+	}
+	
+	private final void visitFor(final FunctionMacro macro) {
+		this.additionalDirectives.append("#define ");
+		this.additionalDirectives.append(macro.getName());
+		this.additionalDirectives.append("(");
+		final EList<String> params = macro.getParams();
+		boolean isFirst = true;
+		for (int i = 0; i < params.size(); i++) {
+			if (!isFirst) {
+				this.additionalDirectives.append(",");
+			}
+			final String param = params.get(i);
+			this.additionalDirectives.append(param);
+			isFirst = false;
+		}
+		this.additionalDirectives.append(")");
+		this.additionalDirectives.append(" ");
+		this.additionalDirectives.append(macro.getValue());
+		this.additionalDirectives.append(System.lineSeparator());
+	}
+	
+	private final void error(final String text) {
+		System.err.println("at.jku.weiner.kefax.main: text='" + text + "'");
+		throw new RuntimeException(text);
+	}
+
+	public String getAdditionalDirectivesAsString() {
+		return this.additionalDirectives.toString();
+	}
+	
+	// private boolean isStandardInclude(final Model model) {
+	// // TODO Auto-generated method stub
+	// return false;
+	// }
+}
--- workspace/at.jku.isse.ecco.kefax.main/src/at/jku/isse/ecco/kefax/main/startup/Main.java
+++ workspace/at.jku.isse.ecco.kefax.main/src/at/jku/isse/ecco/kefax/main/startup/Main.java
@@ -5,21 +5,141 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ui.IStartup;
+import org.eclipse.xtext.resource.IResourceFactory;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.ui.resource.XtextResourceSetProvider;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
+import org.eclipse.xtext.validation.Issue;
+
+import com.google.inject.Injector;
 
 import at.jku.isse.ecco.kefax.dotconfig.dotconfig.Config;
+import at.jku.weiner.c.cmdarguments.cmdArgs.CmdLine;
+import at.jku.weiner.c.cmdarguments.cmdArgs.Model;
+import at.jku.weiner.c.cmdarguments.ui.internal.CmdArgsActivator;
 
 public class Main implements IStartup {
-
+	
 	@Override
 	public void earlyStartup() {
 		try {
-			this.run();
-		} catch (final IOException e) {
-			e.printStackTrace();
+			Thread.sleep(1000);
+			System.out.println("at.jku.weiner.kefax.main - Start of program!");
+			// this.run();
+			this.run2();
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("at.jku.weiner.kefax.main - End of program!");
+	}
+
+	private void run2() throws Exception {
+		final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		final IProject project = root.getProject("HelloC");
+		final IResource cmdRes = project.findMember("test.cmd");
+		if ((cmdRes == null) || !cmdRes.isAccessible()
+				|| !(cmdRes instanceof IFile)) {
+			this.error("invalid cmd file='" + cmdRes + "'!");
+		}
+		final IFile cmdFile = (IFile) cmdRes;
+		final String cmdPath = cmdRes.getLocationURI().toString();
+		System.out.println("cmdPath='" + cmdPath + "'");
+		// get command arguments Xtext units
+		final CmdArgsActivator activator = CmdArgsActivator.getInstance();
+		if (activator == null) {
+			this.error("activator is null!");
+		}
+		final Injector injector = activator
+				.getInjector(CmdArgsActivator.AT_JKU_WEINER_C_CMDARGUMENTS_CMDARGS);
+		if (injector == null) {
+			this.error("injector is null!");
+		}
+		// load resource
+		final Resource res = this.loadResource(injector, cmdFile);
+		if (res == null) {
+			this.error("res is null!");
+		}
+		// validate resource
+		this.validateResource(injector, res);
+		System.out.println("validation has been successfull!");
+		// get command arguments
+		final EObject object = res.getContents().get(0);
+		if ((object == null) || (!(object instanceof Model))) {
+			this.error("Returned object of file='" + cmdPath
+					+ "' from XText is null!");
+		}
+		final Model model = (Model) object;
+		final EList<CmdLine> cmdLines = model.getLines();
+		if ((cmdLines == null) || cmdLines.isEmpty()) {
+			this.error("no command lines found in command file='" + cmdPath
+					+ "'");
+		}
+		for (int i = 0; i < cmdLines.size(); i++) {
+			final CmdLine cmdLine = cmdLines.get(i);
+			final CmdArgs args = new CmdArgs(cmdPath, cmdLine);
+			final String additionalDirectives = args
+					.getAdditionalDirectivesAsString();
+			System.out.println("additionalDirectives='" + additionalDirectives
+					+ "'");
+			// final boolean stdInclude = this.isStandardInclude(model);
 		}
 	}
 
+	private final Resource loadResource(final Injector injector,
+			final IFile iFile) throws Exception {
+		final IResourceFactory resourceFactory = injector
+				.getInstance(IResourceFactory.class);
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				".cmd", resourceFactory);
+
+		final IProject iProject = iFile.getProject();
+		final XtextResourceSetProvider provider = injector
+				.getInstance(XtextResourceSetProvider.class);
+		final XtextResourceSet resourceSet = (XtextResourceSet) provider
+				.get(iProject);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL,
+				Boolean.TRUE);
+		final URI uri = URI.createURI(iFile.getLocationURI().toString());
+		// final String uriStr = uri.toFileString();
+		// System.out.println("uri.toFileString()='" + uriStr + "'");
+		// System.out.println("uri.toString()='" + uri.toString() + "'");
+		final Resource resource = resourceSet.getResource(uri, true);
+		return resource;
+	}
+	
+	private final void validateResource(final Injector injector,
+			final Resource resource) throws Exception {
+		// validate the resource
+		final IResourceValidator validator = injector
+				.getInstance(IResourceValidator.class);
+		final List<Issue> list = validator.validate(resource, CheckMode.ALL,
+				CancelIndicator.NullImpl);
+		if (!(list.isEmpty())) {
+			final String uri = resource.getURI().toFileString();
+			this.error("Errors found during validation for resource='" + uri
+					+ "': " + list.toString());
+		}
+	}
+
+	private void error(final String text) {
+		System.err.println("at.jku.weiner.kefax.main: text='" + text + "'");
+		throw new RuntimeException(text);
+	}
+
+	@SuppressWarnings("unused")
 	private void run() throws IOException {
 		final File linuxSrcDir = Settings.DEFAULT_LINUX_DIR;
 		final String linuxSrcDirPath = linuxSrcDir.getAbsolutePath();

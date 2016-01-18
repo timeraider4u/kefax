f6f4afd5d3ac2bc1af1f51aa5df9755cb2b5d96e
timeraider@gmx.at <timeraider@gmx.at>
2016-01-18 22:12:29 +0100

    small fix

--- workspace/at.jku.weiner.c.preprocess/src/at/jku/weiner/c/preprocess/utils/macros/DefinitionTable.java
+++ workspace/at.jku.weiner.c.preprocess/src/at/jku/weiner/c/preprocess/utils/macros/DefinitionTable.java
@@ -12,30 +12,30 @@ import at.jku.weiner.c.preprocess.utils.LexerUtils;
 import at.jku.weiner.c.preprocess.utils.MyLog;
 
 public final class DefinitionTable {
-
+	
 	private long id = -1;
 	private final Map<String, DefinitionMacro> macros = new HashMap<String, DefinitionMacro>();
 	protected final LexerUtils lexer;
-
+	
 	public DefinitionTable(final LexerUtils lexer) {
 		this.lexer = lexer;
 	}
-
+	
 	public void reset() {
 		this.macros.clear();
 		MyLog.trace("DefinitionTable.reset()");
 	}
-
+	
 	public int size() {
 		return this.macros.size();
 	}
-
+	
 	public boolean isDefined(final String macroName) {
 		boolean result = false;
 		result = this.macros.containsKey(macroName);
 		return result;
 	}
-
+	
 	public void add(final String id, final String replaceWith) {
 		final String key = id;
 		final String val = replaceWith;
@@ -46,7 +46,7 @@ public final class DefinitionTable {
 		this.checkForExistence(key, newMacro);
 		this.macros.put(id, newMacro);
 	}
-
+	
 	private void checkForExistence(final String key,
 			final DefinitionMacro newMacro) {
 		if (this.macros.containsKey(key)) {
@@ -59,21 +59,21 @@ public final class DefinitionTable {
 			}
 		}
 	}
-
+	
 	public void addFunctionMacro(final String id, final IdentifierList list,
 			final String replaceWith) {
 		final String key = id;
 		final DefinitionMacro newMacro = new DefinitionFunctionMacro(this, key,
 				list, replaceWith);
 		this.checkForExistence(key, newMacro);
-
+		
 		this.macros.put(id, newMacro);
 	}
-
+	
 	public void remove(final String key) {
 		this.macros.remove(key);
 	}
-
+	
 	public boolean containsAKey(final String code) {
 		final boolean result = false;
 		final List<Token> list = this.lexer.getTokens(code);
@@ -89,7 +89,7 @@ public final class DefinitionTable {
 		}
 		return result;
 	}
-
+	
 	public String fullResolve(final String code) {
 		final StringBuffer result = new StringBuffer("");
 		final List<Token> list = this.lexer.getTokens(code);
@@ -106,11 +106,11 @@ public final class DefinitionTable {
 				+ "', result='" + resultStr + "'");
 		return resultStr;
 	}
-
+	
 	protected int resolve(final long parenID, final List<Token> list,
 			final int start, final int stop) {
 		int i = start;
-		for (i = start; (i < stop); i++) {
+		for (i = start; (i < stop) && (i < list.size()); i++) {
 			final Token next = list.get(i);
 			final String text = next.getText();
 			MyLog.trace("resolve: parenID=" + parenID + "', token('" + i
@@ -130,5 +130,5 @@ public final class DefinitionTable {
 		}
 		return i;
 	}
-
+	
 }

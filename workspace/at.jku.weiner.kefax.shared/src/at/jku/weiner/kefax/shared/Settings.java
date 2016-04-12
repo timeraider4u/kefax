eaba978361231658d374d072f25391b4e5a14325
timeraider@gmx.at <timeraider@gmx.at>
2016-04-12 03:49:08 +0200

    added new (empty) plugin-projects for kefax operations

---
+++ workspace/at.jku.weiner.kefax.shared/src/at/jku/weiner/kefax/shared/Settings.java
@@ -0,0 +1,8 @@
+package at.jku.weiner.kefax.shared;
+
+public final class Settings {
+
+	public static final String LINUX_CHECKOUT_PRJ = "kefax-linux-source";
+	public static final String LINUX_CHECKOUT_DIR = "src";
+
+}

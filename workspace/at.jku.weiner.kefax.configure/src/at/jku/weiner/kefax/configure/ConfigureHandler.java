6cb0bfbbc9ac055897065539d0bfb5c4b1fe9a1f
timeraider@gmx.at <timeraider@gmx.at>
2016-04-15 03:53:31 +0200

    configure-project set-up

---
+++ workspace/at.jku.weiner.kefax.configure/src/at/jku/weiner/kefax/configure/ConfigureHandler.java
@@ -0,0 +1,15 @@
+package at.jku.weiner.kefax.configure;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ConfigureHandler extends AbstractHandler {
+	
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

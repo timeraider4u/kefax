27791fdba95a560b93b1d68db73e358a394c7ab2
Harald Timeraider <timeraider@gmx.at>
2016-05-02 14:27:00 +0200

    fix for demo2

--- workspace/at.jku.weiner.kefax.main/src/at/jku/weiner/kefax/main/menu/Demo2Handler.java
+++ workspace/at.jku.weiner.kefax.main/src/at/jku/weiner/kefax/main/menu/Demo2Handler.java
@@ -3,7 +3,7 @@ package at.jku.weiner.kefax.main.menu;
 public class Demo2Handler extends DemoHandler {
 
 	public Demo2Handler() {
-		super("at.jku.weiner.kefax.main.Demo2Cmd", true);
+		super("at.jku.weiner.kefax.main.Demo2Cmd", false);
 	}
 
 }
